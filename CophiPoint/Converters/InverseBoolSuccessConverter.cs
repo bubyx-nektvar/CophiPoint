a0e92654e1168949ba470b4104540f07939179d6
Filip Havel <fil.havel@gmail.com>
2020-02-15 20:03:07 +0100

    -fix slide up positions

---
+++ CophiPoint/Converters/InverseBoolSuccessConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CophiPoint.Converters
+{
+    public class InverseBoolSuccessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var v = (bool?)value;
+            return v.HasValue ? !v.Value : true;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
