f7998135d0965c7ce2ed63b5eecd78a358fdffc8
bubyx <fil.havel@gmail.com>
2020-02-15 16:02:36 +0100

    cleanup files

--- TestPhpServer/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: filha
- * Date: 21.09.2019
- * Time: 12:22
- */
-$fullUrl = 'mff.cophipoint://connect';
-header("Location: ".$fullUrl."?".$_SERVER['QUERY_STRING']);
