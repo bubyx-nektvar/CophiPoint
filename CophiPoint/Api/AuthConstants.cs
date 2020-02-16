3d65c39e670bdb86274dca97cc67dbc73b4db051
bubyx <fil.havel@gmail.com>
2020-02-16 22:00:28 +0100

    cleanup

--- CophiPoint/Api/AuthConstants.cs
+++ CophiPoint/Api/AuthConstants.cs
@@ -6,11 +6,12 @@ namespace CophiPoint.Api
 {
     public static class AuthConstants
     {
-        public const string ClientId = "CophiPointAPI";
-        public const string ClientSecret = "d763c6623386636b42608de9856731edbca26885540b4de96cdc8d4d";
+        public const string ClientId = "TODO";//TODO customize
+        public const string ClientSecret = "TODO";//TODO customize
+        public const string RedirectUri = "TODO";//TODO customize
+
         public static string Scope => string.Join(" ",ScopesArray); 
         public static string[] ScopesArray = new[] { "openid","email","balance" };
-        public const string RedirectUri = "mff.cophipoint://connect";
         public const int ExpirationPeriodMinutes = 60;
     }
 }
--- TestPhpServer/config/MojeId.php
+++ TestPhpServer/config/MojeId.php
@@ -9,13 +9,10 @@
 class MojeId
 {
     //bubyx
-    const ClientId = 'Ux9KtBJLZVIn';
-    const ClientSecret = '8e683dfabd834846ac00d5c99d9be55271d15e68d89c6089af8ec459';
-    //192.168.1.168 (localhost)
-//    const ClientId = 'vK3UM0RApi3y';
-//    const ClientSecret = 'f265127d430a1b0fc697a592137882bf7660764426e3fcff54e5cae0';
-
-    const ProviderUrl = 'https://mojeid.regtest.nic.cz';
+    const ClientId = 'TODO'; //TODO customize
+    const ClientSecret = 'TODO'; //TODO customize
+    const ProviderUrl = 'TODO'; //TODO customize
+    
     const Scopes = "email openid";
     const Claims = array(
         "userinfo"=> array(
